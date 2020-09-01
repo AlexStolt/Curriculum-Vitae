5c35043d5d462e53dae66f57cc4a54103add4313
AlexStolt <alexstolt0131@gmail.com>
2020-09-01 18:39:46 +0300

    Added Education and Training, Issues (Scroll on div)

---
+++ client/scripts/scroll.js
@@ -0,0 +1,22 @@
+//Get Height of Header (Navigation Bar)
+//To Calculate Nav Bar Height I calculate the Height from top of the first section element, which is "Professional Summary" (id="#summary")
+const menuItems = document.getElementsByClassName('menu-item');
+const sections = document.getElementsByTagName('section');
+var offset = document.getElementById('summary').offsetHeight;
+
+console.log(typeof(menuItems))
+
+
+
+
+
+
+
+
+
+
+
+competencies.onclick = (event) => {
+    event.preventDefault();
+    
+}   
