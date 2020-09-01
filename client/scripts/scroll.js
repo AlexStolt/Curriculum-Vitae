016b9c0c413c9261f0a2552136c6cd469f6da8da
AlexStolt <alexstolt0131@gmail.com>
2020-09-01 20:42:18 +0300

    Added Smooth Scroll, [TASKS] -> FOOTER, Test on Firefox, Add Text

--- client/scripts/scroll.js
+++ client/scripts/scroll.js
@@ -1,22 +1,17 @@
 //Get Height of Header (Navigation Bar)
 //To Calculate Nav Bar Height I calculate the Height from top of the first section element, which is "Professional Summary" (id="#summary")
-const menuItems = document.getElementsByClassName('menu-item');
-const sections = document.getElementsByTagName('section');
-var offset = document.getElementById('summary').offsetHeight;
+const menuItems = document.querySelectorAll('.menu-item');
+const sections = document.querySelectorAll('section');
+const navigationBarHeight = document.getElementById('nav-bar').offsetHeight;
 
-console.log(typeof(menuItems))
-
-
-
-
-
-
-
-
-
-
-
-competencies.onclick = (event) => {
-    event.preventDefault();
-    
-}   
+menuItems.forEach((item, index) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.scroll(0, sections[index].offsetTop - navigationBarHeight - 14);
+        //From sidebar.js
+        if(active){
+            menu.style.width = "0";
+            active = false;
+        }
+    });
+});
